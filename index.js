93e048bf6892c7814950eeeb371b7abfe85549ec
smazad1212 <smazad1212@gmail.com>
2019-09-04 17:43:36 +0600

    sequelize added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { makeExecutableSchema } from 'graphql-tools';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-export const schema = makeExecutableSchema({
+const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
